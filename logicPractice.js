24b844e3c43003e108db054a8d1d21c535603aa5
electroayan14 <ayanavalahiri14@gmail.com>
2025-08-02 09:57:37 +0530

    new questions added

--- logicPractice.js
+++ logicPractice.js
@@ -1,47 +1,57 @@
-// let gameNumber = 30;
-// let givenNumber = prompt("Guess the number");
-// while (givenNumber != gameNumber) {
-//     givenNumber = prompt("You have entered wrong number,Guess the right number");
-// }
-// console.log("Number matched");
+let gameNumber = 30;
+let givenNumber = prompt("Guess the number");
+while (givenNumber != gameNumber) {
+    givenNumber = prompt("You have entered wrong number,Guess the right number");
+}
+console.log("Number matched");
 
-// let userName = prompt("Enter the username");
-// console.log("@" + userName + userName.length);
-
-
-
+let userName = prompt("Enter the username");
+console.log("@" + userName + userName.length);
 //For a given array with marks of students-> [85, 97, 44, 37, 76, 60].Find the average marks of the entire class.
-// let marks = [5, 97, 44, 37, 76, 60];
-// let sum = 0;
-// for (let mark of marks) {
-//     sum += mark;
-// }
-// console.log(sum / marks.length);
+let marks = [5, 97, 44, 37, 76, 60];
+let sum = 0;
+for (let mark of marks) {
+    sum += mark;
+}
+console.log(sum / marks.length);
 
 //For a given array with prices of 5 items -> [250, 645, 300, 900, 50] All items have an offer of 10% OFF on them. Change the array to store final price after applying offer.
-// let items = [250, 645, 300, 900, 50];
-// for (let i = 0; i < items.length; i++) {
-//     console.log(`value of the item ${items[i]}`);
-//     let offer = items[i] / 10;
-//     items[i] = items[i] - offer;
-// }
-// console.log(items);
+let items = [250, 645, 300, 900, 50];
+for (let i = 0; i < items.length; i++) {
+    console.log(`value of the item ${items[i]}`);
+    let offer = items[i] / 10;
+    items[i] = items[i] - offer;
+}
+console.log(items);
 //Return all even numbers from an array
-// let numbers=[2,4,22,35,77,34,98,80,55,53,56];
-// for(let i=0;i<numbers.length;i++){
-//    if(i%2==0){
-//     let evenNumber=i;
-//     numbers[i]=evenNumber;
-//    }
-// }
-// console.log(numbers);
+let numbers=[2,4,22,35,77,34,98,80,55,53,56];
+for(let i=0;i<numbers.length;i++){
+   if(i%2==0){
+    let evenNumber=i;
+    numbers[i]=evenNumber;
+   }
+}
+console.log(numbers);
 
 //Find the largest number in an array           
-// let findNumbers = [40, 4, 55, 78, 98, 100, 34, 120, 67, 88, 35];
-// let largest=findNumbers[0];
-// for(let number of findNumbers){
-//     if(number>largest){
-//         largest=number;
-//     }
-// }
-// console.log(largest);
+let findNumbers = [40, 4, 55, 78, 98, 100, 34, 120, 67, 88, 35];
+let largest=findNumbers[0];
+for(let number of findNumbers){
+    if(number>largest){
+        largest=number;
+    }
+}
+console.log(largest);
+
+//Count how many times a value appears in an array
+let arr=[2,4,3,45,55,5,56,50,2,4,80,8,8];
+let count={};
+for(let number of arr){
+    if(count[number]){
+        count[number]+=1;
+    }else{
+        count[number]=1;
+    }
+}
+console.log(count);
+
